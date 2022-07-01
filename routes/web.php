7274fdbccedcb8acecb868e1ee3c6437925a4d96
Practicante de Logistica Pastas Comarrico <NCPAprdelopc@ads.loc>
2022-07-01 08:50:33 -0500

    Manejo de rutas por roles

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,14 @@ use Illuminate\Support\Facades\Auth;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('/edit', [App\Http\Controllers\HomeController::class, 'edit'])->name('edit');
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'admin'])->name('admin');
 
 
 
